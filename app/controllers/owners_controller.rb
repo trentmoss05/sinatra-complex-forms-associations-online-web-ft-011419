ca75d3a00cf6f0c0353e6a718d062d025336902a
Trent Moss <trentmoss05@gmail.com>
2019-03-07 17:21:13 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -30,10 +30,9 @@ class OwnersController < ApplicationController
   end
 
   patch '/owners/:id' do
-    patch '/owners/:id' do
     ####### bug fix
     if !params[:owner].keys.include?("pet_ids")
-    params[:owner]["pet_ids"] = []
+      params[:owner]["pet_ids"] = []
     end
     #######
 
