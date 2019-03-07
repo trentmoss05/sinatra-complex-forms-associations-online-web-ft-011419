a8301a1a6edb22ce508e6f92ba672a5fbf033bd0
Trent Moss <trentmoss05@gmail.com>
2019-03-07 17:19:08 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,14 +2,14 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
     erb :'/pets/new'
   end
 
-  post '/pets' do 
+  post '/pets' do
     @pet = Pet.create(params[:pet])
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
@@ -18,12 +18,12 @@ class PetsController < ApplicationController
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  patch '/pets/:id' do
 
     redirect to "pets/#{@pet.id}"
   end
